7b2670b3a632e0c9431b7e3fb8b33b7c7089e324
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-06 01:28:15 +0800

    getDecress

---
+++ src/getDecress.js
@@ -0,0 +1,18 @@
+const loadAllItems = require('../src/items')
+const loadPromotions = require('../src/promotions')
+
+const getDecress = (input) => {
+  var items = loadAllItems();
+  var promotions = loadPromotions();
+  var total = 0;
+
+  for (var key in input) {
+      total += items.find(n => n.id === key).price * input[key];
+  }
+
+  input.total = total - 6;
+  input.type = promotions[0].type;
+  return input;
+}
+
+module.exports = getDecress;
